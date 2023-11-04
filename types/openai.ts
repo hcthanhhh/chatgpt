16176fd9e44cf0ac75ee8cb2904d39f2d5137a1f
thanhhc3 <thanhhc3@vng.com.vn>
2023-11-05 04:49:49 +0700

    fix bug

--- types/openai.ts
+++ types/openai.ts
@@ -3,7 +3,7 @@ import { OPENAI_API_TYPE } from '../utils/app/const';
 export interface OpenAIModel {
   id: string;
   name: string;
-  maxLength?: number; // maximum length of a message
+  maxLength: number; // maximum length of a message
   tokenLimit: number;
 }
 
